af57ef9478809cb3af4861bd2e59d45cce24eb48
KoptevaAnastasiia <150849726+KoptevaAnastasiia@users.noreply.github.com>
2024-06-02 19:15:51 +0300

    task01 part02 (rotation scaling mirroring)

--- main.py
+++ main.py
@@ -3,6 +3,51 @@ import matplotlib.pyplot as plt
 
 
 
+def reflection_x():
+    vector[:, 1] *= -1
+    heart[:, 1] *= -1
+
+
+
+
+
+
+def reflection_y():
+    i = 0 ;
+    while i != len(vector):
+        vector[i, 0] *= -1
+
+        i += 1
+    i = 0
+    while i != len(heart):
+        heart[i, 0] *= -1
+
+        i += 1
+
+    # vector[0, :] *= -1
+    # heart[0, :] *= -1
+    #
+
+
+def scaling ():
+    ask = int(input("ask the num "));
+
+    vector[:, 1] *= ask
+    heart[:, 1] *= ask
+
+    i = 0;
+    while i != len(vector):
+        vector[i, 0] *= ask
+
+        i += 1
+    i = 0
+    while i != len(heart):
+        heart[i, 0] *= ask
+
+        i += 1
+
+
+
 def rotation_of_the_object(points, angle):
     rad = np.radians(angle)
     rotation_matrix = np.array([
@@ -12,6 +57,32 @@ def rotation_of_the_object(points, angle):
     return points.dot(rotation_matrix)
 
 
+def rotation_of_the_object_y (points, angle):
+    rad = np.radians(angle)
+    rotation_matrix = np.array([
+        [np.cos(rad), 0],
+        [0, 1]
+    ])
+    return np.dot(points, rotation_matrix)
+
+
+def rotation_of_the_object_x (points, angle):
+    rad = np.radians(angle)
+    rotation_matrix = np.array([
+        [1, 0],
+        [0, np.cos(rad)]
+    ])
+    return np.dot(points, rotation_matrix)
+
+
+
+
+
+
+
+
+
+
 heart = np.array([
     [0, -1], [1, 1], [0.5, 2], [0, 1.5], [-0.5, 2], [-1, 1], [0, -1]
 ])
@@ -19,6 +90,11 @@ heart = np.array([
 vector = np.array([
     [-1, -0.5] ,[-1, -1],[-0.5 , -1] ,[-1, -1], [1, 2]
 ])
+
+
+
+
+
 
 
 
@@ -45,16 +121,62 @@ plt.show()
 ask = int(input('chose'))
 
 
-if ask:
-    rotated_heart = rotation_of_the_object(heart, 50)
-    rotated_vector = rotation_of_the_object(vector, 50)
+if ask == 1:
+    heart = rotation_of_the_object(heart, 50)
+    vector = rotation_of_the_object(vector, 50)
 
-    # Візуалізація обернутих об'єктів з обраним кутом
+
     plt.figure(figsize=(8, 8))  # створює полотно в розмірі 8 на 8 дюймів
-    plt.plot(rotated_heart[:, 0], rotated_heart[:, 1], marker='o' )
-    plt.plot(rotated_vector[:, 0], rotated_vector[:, 1], marker='o' )
+    plt.plot(heart[:, 0],  heart[:, 1], marker='o' )
+    plt.plot(vector[:, 0],  vector[:, 1], marker='o' )
     plt.grid(True)  # будує розмірність на графіку
     plt.axis('equal')  # вирівнює
     plt.show()
+
+if ask == 3:
+    ask = input('x or y')
+    if ask == "x" or ask == "X":
+        reflection_x()
+    if ask == "y" or ask == "Y":
+        reflection_y()
+
+    plt.plot(heart[:, 0], heart[:, 1], marker='o', label='Heart')
+    plt.plot(vector[:, 0], vector[:, 1], marker='o', label='Vector')
+    plt.grid(True)  # будує розмірність на графіку
+    plt.axis('equal')  # вирівнює
+    plt.show()
+
+
+
+
+if ask == 2:
+    scaling()
+    plt.plot(heart[:, 0], heart[:, 1], marker='o', label='Heart')
+    plt.plot(vector[:, 0], vector[:, 1], marker='o', label='Vector')
+    plt.grid(True)  # будує розмірність на графіку
+    plt.axis('equal')  # вирівнює
+    plt.show()
+
+if ask == 4:
+    chose = input('x or y')
+    if chose == "y" or chose == "Y":
+
+        rotated_heart = rotation_of_the_object_y(heart, 75)
+        rotated_vector = rotation_of_the_object_y(vector, 75)
+        plt.plot(rotated_heart[:, 0], rotated_heart[:, 1], marker='o', label='Rotated Heart')
+        plt.plot(rotated_vector[:, 0], rotated_vector[:, 1], marker='o', label='Rotated Vector')
+        plt.grid(True)  # будує розмірність на графіку
+        plt.axis('equal')  # вирівнює
+        plt.legend()
+        plt.show()
+    elif chose == "x" or chose == "X" :
+        rotated_heart = rotation_of_the_object_x(heart, 75)
+        rotated_vector = rotation_of_the_object_x(vector, 75)
+        plt.plot(rotated_heart[:, 0], rotated_heart[:, 1], marker='o', label='Rotated Heart')
+        plt.plot(rotated_vector[:, 0], rotated_vector[:, 1], marker='o', label='Rotated Vector')
+        plt.grid(True)  # будує розмірність на графіку
+        plt.axis('equal')  # вирівнює
+        plt.legend()
+        plt.show()
 
 
